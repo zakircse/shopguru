af6bac774c1227bcd77e323df771d62f181b2fd9
zakircse <zakir.rars@gmail.com>
2022-11-27 22:33:39 +0600

    connected firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopguru/consts/consts.dart';
 import 'package:shopguru/views/splash_screen/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
